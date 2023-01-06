22fd5d9ccb05620e6b7946a0ed6c9c42b34ac390
kazukichi <20309063+n-kazukichi@users.noreply.github.com>
2023-01-06 22:20:55 +0900

    ch2_3

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,24 @@ module.exports = {
   entry: {
     // チャプターごとに定義
     ch01: './src/ch01.ts',
-    ch02: './src/ch02.ts'
-//    ch03: './src/ch03.ts'...
+    ch02: './src/ch02.ts',
+    'ch02-3': './src/ch02-3.ts',
+    //    ch03: './src/ch03.ts'...
   },
   output: {
     // chXX,jsがhtmlディレクトリに配置されるように。
     filename: '[name].js',
-    path: __dirname + '/html'
+    path: __dirname + '/html',
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader'
-      }
-    ]
+        use: 'ts-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: [
-      '.ts'
-    ]
-  }
-};
+    extensions: ['.ts'],
+  },
+}
